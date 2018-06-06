5029aeb1d6863cf95f59517e1e3bfb9e90300f22
JohnCat <john@catnach.co.uk>
2018-06-06 20:31:54 +0100

    correct styles

--- microblog.py
+++ microblog.py
@@ -6,6 +6,3 @@ app = create_app('development')
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Country':Country,'Region':Region,'Brand':Brand,'Type':Type,'Review': Review}
-
-
-app.run()
